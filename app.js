5df86f0bf6204955eee98ef46524a894147a1605
Gordon Lau Wai chung <gordonlauwaichung@gmail.com>
2018-01-05 15:55:50 +0800

    Added handlers and client side javascript.

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+const knexfile = require('./knexfile')[process.env.NODE_ENV || 'development' ]
+const knex = require('knex')(knexfile)
+
 const express = require('express');
 const app = express();
-
+const hb = require('express-handlebars');
+app.engine('handlebars', hb({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 app.use(express.static("public"));
 
 
 app.get('/users', function(req, res) {
-    
+        
 });
+
 
 
 app.listen(8080);
