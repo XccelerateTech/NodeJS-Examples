2cae7d2b26f1054101ab1bdfad5d5fdec36a0164
Gordon Lau Wai chung <gordonlauwaichung@gmail.com>
2018-01-17 17:29:44 +0800

    Added index page.

--- ViewRouter.js
+++ ViewRouter.js
@@ -4,6 +4,7 @@ module.exports = class ViewRouter{
     
     router(){
         const router = express.Router();
+        router.get('/',(req,res)=>res.render("index"));
         router.get('/users',(req,res)=>res.render("users"));
         router.get('/groups',(req,res)=>res.render("groups"));
         return router;
