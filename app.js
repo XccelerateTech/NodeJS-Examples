a4ffee82d2feb33121b3fd91ed2692b4ec963fde
Gordon Lau Wai chung <gordonlauwaichung@gmail.com>
2018-01-18 19:59:33 +0800

    Added required-dir to reduce boilerplate.

--- app.js
+++ app.js
@@ -6,20 +6,18 @@ const isLoggedIn = require('./utils/guard').isLoggedIn;
 
 // Dependency Injection for Routers and Service
 const ViewRouter = require('./ViewRouter');
-const GroupRouter = require('./routers/GroupRouter');
-const UserRouter = require('./routers/UserRouter');
-const SocketIORouter = require('./routers/SocketIORouter');
 
-const GroupService = require('./services/GroupService');
-const UserService = require('./services/UserService');
-const RedisService = require('./services/RedisService');
+const { GroupRouter,
+        UserRouter,
+        SocketIORouter} = require('./routers');
 
+const { GroupService,
+        UserService,
+        RedisService} = require('./services');
 
 let redisService = new RedisService();
 let groupService = new GroupService(knex,redisService);
 let userService = new UserService(knex,redisService);
-
-
 
 const {app,server,io} = require('./utils/init-app')(redisService);
 
